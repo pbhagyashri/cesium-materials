9c9d7729849b4ac18440032027ef17147d26ae63
Bhagyashri Padmawar <bhagyashripadmawar@Bhagyashris-MBP.fios-router.home>
2020-05-27 11:56:48 -0400

    clean up cypress setup

---
+++ cypress/integration/examples/my-e2e-test.js
@@ -0,0 +1,9 @@
+context('Actions', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('.type() - type into a DOM element', () => {
+    // cy.get('.action-email')
+  });
+});
